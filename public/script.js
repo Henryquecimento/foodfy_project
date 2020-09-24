d2d3797d7272de104ff5fb066c5a34a55c2504d9
Henryque <henryque_1@hotmail.com>
2020-09-23 21:58:09 -0300

    projeto refatorado

---
+++ public/script.js
@@ -0,0 +1,29 @@
+const cards = document.querySelectorAll('.card');
+
+// não põe notação-ponto porque 'button' é tag e não classe
+const buttons = document.querySelectorAll('button'); 
+const content = document.querySelectorAll('.content');
+
+
+for (let index = 0; cards.length; index++) {
+    cards[index].addEventListener('click', () => {
+
+        window.location.href = `/recipes/${index}`;
+
+    })
+}
+
+
+for (let [i, button] of buttons.entries()) {
+    button.addEventListener('click', () => {
+        if (content[i].classList.contains('show')) {
+            content[i].classList.remove('show');
+            content[i].classList.add('hide');
+            button.innerHTML = 'Mostrar';
+        } else {
+            content[i].classList.add('show');
+            content[i].classList.remove('hide');
+            button.innerHTML = 'Esconder';
+        }
+    })
+}
