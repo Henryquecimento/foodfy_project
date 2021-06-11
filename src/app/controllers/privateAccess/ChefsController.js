11a9795aa607fe509a38839929aaad4254d07ee9
henryque <henryque_1@hotmail.com>
2021-06-10 21:20:22 -0300

    refactor(ChefsController): error handling pattern to the methods

--- src/app/controllers/privateAccess/ChefsController.js
+++ src/app/controllers/privateAccess/ChefsController.js
@@ -1,78 +1,92 @@
-const Chef = require('../../models/Chef');
+const Chef = require("../../models/Chef");
 
 module.exports = {
-    async index(req, res) {
-        const results = await Chef.all();
-        const chefs = results.rows;
-       
-        try {
-            return res.render('admin/chefs/index', { chefs });
-        } catch (err) {
-            throw new Error({
-                message: `Database Error ${err}`
-            });
-        }
-        
-    },
-    create(req, res) {
-        return res.render('admin/chefs/create');
-    },
-    async post(req, res) {
+	async index(req, res) {
+		try {
+			const results = await Chef.all();
+			const chefs = results.rows;
 
-        const keys = Object.keys(req.body);
+			return res.render("admin/chefs/index", { chefs });
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	create(req, res) {
+		return res.render("admin/chefs/create");
+	},
+	async post(req, res) {
+		try {
+			const keys = Object.keys(req.body);
 
-        for (let key of keys) {
-            if (req.body[key] == '') {
-                return res.send("Please, fill all the fields!");
-            }
-        }
+			for (let key of keys) {
+				if (req.body[key] == "") {
+					return res.send("Please, fill all the fields!");
+				}
+			}
 
-        const results = await Chef.create(req.body);
-        const chefId = results.rows[0].id;
+			const results = await Chef.create(req.body);
+			const chefId = results.rows[0].id;
 
-        return res.redirect(`/admin/chefs/${chefId}`);
+			return res.redirect(`/admin/chefs/${chefId}`);
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async show(req, res) {
+		try {
+			let results = await Chef.find(req.params.id);
+			const chef = results.rows[0];
 
-    },
-    async show(req, res) {
-        let results = await Chef.find(req.params.id);
-        const chef = results.rows[0];
-        
-        results = await Chef.findRecipe(req.params.id);
-        const recipes = results.rows;
+			results = await Chef.findRecipe(req.params.id);
+			const recipes = results.rows;
 
-        return res.render('admin/chefs/show', { chef, recipes });
-    },
-    async edit(req, res) {
-        const results = await Chef.find(req.params.id);
-        const chef = results.rows[0];
+			return res.render("admin/chefs/show", { chef, recipes });
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async edit(req, res) {
+		try {
+			const results = await Chef.find(req.params.id);
+			const chef = results.rows[0];
 
-        return res.render('admin/chefs/edit', { chef });
-    
-    },
-    async put(req, res) {
+			return res.render("admin/chefs/edit", { chef });
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async put(req, res) {
+		try {
+			const keys = Object.keys(req.body);
 
-        const keys = Object.keys(req.body);
+			for (let key of keys) {
+				if (req.body[key] == "") {
+					return res.send("Please, fill all the fields!");
+				}
+			}
 
-        for (let key of keys) {
-            if (req.body[key] == '') {
-                return res.send("Please, fill all the fields!");
-            }
-        }
+			await Chef.update(req.body);
 
-        await Chef.update(req.body);
+			return res.redirect(`/admin/chefs/${req.body.id}`);
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async delete(req, res) {
+		try {
+			//let
+			const results = await Chef.find(req.body.id);
+			const chef = results.rows[0];
 
-        return res.redirect(`/admin/chefs/${req.body.id}`);       
-    },
-    async delete(req, res) {
-        let results = await Chef.find(req.body.id);
-        const chef = results.rows[0];
+			if (chef.total_recipes != 0) {
+				return res.send("Chef possui receitas! Você não pode apagá-lo(a)!");
+			} else {
+				await Chef.delete(req.body.id);
 
-        if ( chef.total_recipes != 0 ) {
-            return res.send('Chef possui receitas! Você não pode apagá-lo(a)!');
-        } else {
-            await Chef.delete(req.body.id);
-
-            return res.redirect('/admin/chefs');
-        }
-    }
-}
+				return res.redirect("/admin/chefs");
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+};
