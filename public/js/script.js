95505ef9856cd653e088fdfa0ff9f65c09d95702
henryque <henryque_1@hotmail.com>
2021-06-11 15:50:35 -0300

    feat: add PhotosUpload and handleFileInputmethod

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,71 @@
 /* -- Hide and Show buttons functionality -- */
+function hideAndShow() {
+  const buttons = document.querySelectorAll("button.hide-button");
+  const content = document.querySelectorAll(".content");
 
-const buttons = document.querySelectorAll("button.hide-button");
-const content = document.querySelectorAll(".content");
-
-for (let [i, button] of buttons.entries()) {
-  button.addEventListener("click", () => {
-    if (content[i].classList.contains("show")) {
-      content[i].classList.remove("show");
-      content[i].classList.add("hide");
-      button.innerHTML = "Mostrar";
-    } else {
-      content[i].classList.add("show");
-      content[i].classList.remove("hide");
-      button.innerHTML = "Esconder";
-    }
-  });
+  for (let [i, button] of buttons.entries()) {
+    button.addEventListener("click", () => {
+      if (content[i].classList.contains("show")) {
+        content[i].classList.remove("show");
+        content[i].classList.add("hide");
+        button.innerHTML = "Mostrar";
+      } else {
+        content[i].classList.add("show");
+        content[i].classList.remove("hide");
+        button.innerHTML = "Esconder";
+      }
+    });
+  }
 }
+hideAndShow();
 
-/* -- Selected Links -- */
-const currentPage = location.pathname;
-const menuItems = document.querySelectorAll('header .links a');
+function activeMenu() {
+  /* -- Selected Links -- */
+  const currentPage = location.pathname;
+  const menuItems = document.querySelectorAll('header .links a');
 
-for (item of menuItems) {
-  if (currentPage.includes(item.getAttribute('href'))) {
-    item.classList.add('active');
+  for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute('href'))) {
+      item.classList.add('active');
+    }
+  }
+}
+activeMenu();
+
+const PhotosUpload = {
+  uploadLimit: 5,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+    const { uploadLimit } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} arquivos!`);
+      event.preventDefault();
+      return
+    }
+
+    Array.from(fileList).forEach(file => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const image = new Image();
+        image.src = String(reader.result);
+
+        const div = document.createElement('div');
+        div.classList.add('photo');
+
+        div.onclick = () => alert('remover foto');
+
+        div.appendChild(image);
+
+        document.querySelector("#photos-preview").appendChild(div);
+
+      }
+
+      reader.readAsDataURL(file)
+
+    })
+
+
   }
 }
