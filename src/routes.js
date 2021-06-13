d68b089dd470834461fd895959cf29a67dfe60e1
henryque <henryque_1@hotmail.com>
2021-06-13 12:29:28 -0300

    chore: multer config finished

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routes = express.Router();
+const multer = require('./app/middlewares/multer');
 const public = require("./app/controllers/publicAccess/PublicController");
 const recipes = require("./app/controllers/privateAccess/RecipesController");
 const chefs = require("./app/controllers/privateAccess/ChefsController");
@@ -20,8 +21,8 @@ routes.get("/admin/recipes/create", recipes.create);
 routes.get("/admin/recipes/:id", recipes.show);
 routes.get("/admin/recipes/:id/edit", recipes.edit);
 
-routes.post("/admin/recipes", recipes.post);
-routes.put("/admin/recipes", recipes.put);
+routes.post("/admin/recipes", multer.array("photos", 5), recipes.post);
+routes.put("/admin/recipes", multer.array("photos", 5), recipes.put);
 routes.delete("/admin/recipes", recipes.delete);
 
 routes.get("/admin/chefs", chefs.index);
@@ -29,8 +30,8 @@ routes.get("/admin/chefs/create", chefs.create);
 routes.get("/admin/chefs/:id", chefs.show);
 routes.get("/admin/chefs/:id/edit", chefs.edit);
 
-routes.post("/admin/chefs", chefs.post);
-routes.put("/admin/chefs", chefs.put);
+routes.post("/admin/chefs", multer.array("photos", 5), chefs.post);
+routes.put("/admin/chefs", multer.array("photos", 5), chefs.put);
 routes.delete("/admin/chefs", chefs.delete);
 
 module.exports = routes;
