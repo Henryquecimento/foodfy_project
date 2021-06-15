6ab07aab907526d8ef8a385aac55b0efa7ce78df
henryque <henryque_1@hotmail.com>
2021-06-15 16:29:43 -0300

    feat(script): add ImageGallery method

--- public/js/script.js
+++ public/js/script.js
@@ -143,3 +143,18 @@ const PhotosUpload = {
     photoDiv.remove();
   }
 }
+
+const ImageGallery = {
+  highlight: document.querySelector('.food-image .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
+  setImage(event) {
+    const { target } = event;
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
+
+    target.classList.add('active');
+
+    ImageGallery.highlight.src = target.src
+
+  }
+}
