b728e21c9a1c2ddba227a680a338df712c055855
henryque <henryque_1@hotmail.com>
2021-07-25 20:00:47 -0300

    refactor(routes): readjust routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,27 +1,43 @@
 const express = require("express");
 const routes = express.Router();
 const multer = require('../app/middlewares/multer');
-const recipes = require("../app/controllers/privateAccess/RecipesController");
-const chefs = require("../app/controllers/privateAccess/ChefsController");
+const RecipesController = require("../app/controllers/privateAccess/RecipesController");
+const ChefsController = require("../app/controllers/privateAccess/ChefsController");
+const ProfileController = require("../app/controllers/privateAccess/ChefsController");
+const UserController = require("../app/controllers/privateAccess/ChefsController");
 
 /* -- Private Access -- */
 
-routes.get("/recipes", recipes.index);
-routes.get("/recipes/create", recipes.create);
-routes.get("/recipes/:id", recipes.show);
-routes.get("/recipes/:id/edit", recipes.edit);
+routes.get("/recipes", RecipesController.index);
+routes.get("/recipes/create", RecipesController.create);
+routes.get("/recipes/:id", RecipesController.show);
+routes.get("/recipes/:id/edit", RecipesController.edit);
 
-routes.post("/recipes", multer.array("photos", 5), recipes.post);
-routes.put("/recipes", multer.array("photos", 5), recipes.put);
-routes.delete("/recipes", recipes.delete);
+routes.post("/recipes", multer.array("photos", 5), RecipesController.post);
+routes.put("/recipes", multer.array("photos", 5), RecipesController.put);
+routes.delete("/recipes", RecipesController.delete);
 
-routes.get("/chefs", chefs.index);
-routes.get("/chefs/create", chefs.create);
-routes.get("/chefs/:id", chefs.show);
-routes.get("/chefs/:id/edit", chefs.edit);
+routes.get("/chefs", ChefsController.index);
+routes.get("/chefs/create", ChefsController.create);
+routes.get("/chefs/:id", ChefsController.show);
+routes.get("/chefs/:id/edit", ChefsController.edit);
 
-routes.post("/chefs", multer.array("photos", 5), chefs.post);
-routes.put("/chefs", multer.array("photos", 5), chefs.put);
-routes.delete("/chefs", chefs.delete);
+routes.post("/chefs", multer.array("photos", 5), ChefsController.post);
+routes.put("/chefs", multer.array("photos", 5), ChefsController.put);
+routes.delete("/chefs", ChefsController.delete);
+
+/* -- USERS -- */
+
+// Rotas de perfil de um usuário logado
+//routes.get('/admin/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
+//routes.put('/admin/profile', ProfileController.put)// Editar o usuário logado
+
+// Rotas que o administrador irá acessar para gerenciar usuários
+// routes.get('/admin/users', UserController.list) // Mostrar a lista de usuários cadastrados
+// routes.post('/admin/users', UserController.post) // Cadastrar um usuário
+// routes.get('/admin/users/create', UserController.create) // Mostrar o formulário de criação de um usuário
+// routes.put('/admin/users/:id', UserController.put) // Editar um usuário
+// routes.get('/admin/users/:id/edit', UserController.edit) // Mostrar o formulário de edição de um usuário
+// routes.delete('/admin/users/:id', UserController.delete) // Deletar um usuário
 
 module.exports = routes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const routes = express.Router();
-const public = require("../app/controllers/publicAccess/PublicController");
+const PublicController = require("../app/controllers/publicAccess/PublicController");
 
 const admin = require('./admin');
 
 routes.use("/admin", admin);
 
 /* -- Public Access -- */
-routes.get("/", public.index);
-routes.get("/about", public.about);
-routes.get("/recipes", public.recipes);
-routes.get("/recipes/:id", public.show);
-routes.get("/chefs", public.chefs);
-routes.get("/chefs/:id", public.showChef);
+routes.get("/", PublicController.index);
+routes.get("/about", PublicController.about);
+routes.get("/recipes", PublicController.recipes);
+routes.get("/recipes/:id", PublicController.show);
+routes.get("/chefs", PublicController.chefs);
+routes.get("/chefs/:id", PublicController.showChef);
 
 module.exports = routes;
