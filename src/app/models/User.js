76fc96fb11288c9177448f972e7e2e2cedac3de3
henryque <henryque_1@hotmail.com>
2021-08-04 18:05:50 -0300

    feat(User): add post method

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -5,5 +5,27 @@ module.exports = {
         return db.query(`
             SELECT * FROM users
         `);
+    },
+    post({ name, email, isAdmin }) {
+        const query = `
+            INSERT INTO users (
+                name,
+                email,
+                password,
+                isAdmin
+            ) VALUES ($1, $2, $3, $4)
+            RETURNING id     
+        `;
+
+        const newPassword = 1234
+
+        const values = [
+            name,
+            email,
+            newPassword,
+            isAdmin
+        ];
+
+        return db.query(query, values);
     }
 }
