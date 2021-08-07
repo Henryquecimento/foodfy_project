3e578247216d56a4c339733e628565a6ace006b0
henryque <henryque_1@hotmail.com>
2021-08-07 16:50:35 -0300

    Enable delete routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -53,6 +53,6 @@ routes.get('/users/create', UserController.create) // Mostrar o formulário de c
 routes.post('/users', UserServices.post, UserController.post) // Cadastrar um usuário
 routes.get('/users/:id/edit', UserController.edit) // Mostrar o formulário de edição de um usuário
 routes.put('/users', UserController.put) // Editar um usuário
-// routes.delete('/users/:id', UserController.delete) // Deletar um usuário
+routes.delete('/users', UserController.delete) // Deletar um usuário
 
 module.exports = routes;
