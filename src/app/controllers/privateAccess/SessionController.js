c0153b84fa226e3bf25a9a8f59a1ae72be306a1d
henryque <henryque_1@hotmail.com>
2021-08-10 17:15:58 -0300

    feat(SessionController): add login method receiand adding user.id in session

--- src/app/controllers/privateAccess/SessionController.js
+++ src/app/controllers/privateAccess/SessionController.js
@@ -3,5 +3,11 @@
 module.exports = {
   loginForm(req, res) {
     return res.render('admin/session/login');
+  },
+  login(req, res) {
+    req.session.userId = req.user.id;
+
+    return res.redirect('/admin/profile');
   }
 }
+
