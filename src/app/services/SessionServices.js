913a8f06a3e70231c10f82cbd1ab914ac154041d
henryque <henryque_1@hotmail.com>
2021-08-10 17:03:05 -0300

    feat(SessionServices): add login method

---
+++ src/app/services/SessionServices.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const { compare } = require('bcrypt');
+
+async function login(req, res, next) {
+  const { email, password } = req.body;
+
+  const user = User.findOne({
+    where: { email }
+  });
+
+  if (!user) return res.send('User does not already exists!');
+
+  const passed = '1234';
+
+  if (!passed) return res.send('Invalid Password, please try again!');
+
+  req.user = user;
+
+  next();
+}
+
+module.exports = {
+  login
+}
