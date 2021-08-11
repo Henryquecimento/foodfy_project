22a1b487e83fed7b88a5510c0e57bb524e17624d
henryque <henryque_1@hotmail.com>
2021-08-11 18:01:54 -0300

    feat(SessionController): add logout method

--- src/app/controllers/privateAccess/SessionController.js
+++ src/app/controllers/privateAccess/SessionController.js
@@ -1,5 +1,4 @@
 
-
 module.exports = {
   loginForm(req, res) {
     return res.render('admin/session/login');
@@ -8,6 +7,13 @@ module.exports = {
     req.session.userId = req.user.id;
 
     return res.redirect('/admin/profile');
+  },
+  logout(req, res) {
+    console.log(req.session);
+    req.session.destroy();
+
+
+    return res.redirect('/');
   }
 }
 
