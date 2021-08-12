fecb6b9e5090ab44fa69fb84a976be0ad7b9afd2
henryque <henryque_1@hotmail.com>
2021-08-12 18:01:40 -0300

    feat(ProfileController): asdd put method to update user profile data

--- src/app/controllers/privateAccess/ProfileController.js
+++ src/app/controllers/privateAccess/ProfileController.js
@@ -1,12 +1,21 @@
 
 module.exports = {
   async index(req, res) {
-    
+
     const { user } = req;
 
     return res.render('admin/profile/edit.njk', { user });
   },
   async put(req, res) {
+    const { user } = req;
 
+    const { name, email } = req.body;
+
+    await User.update(user.id, {
+      name,
+      email
+    });
+
+    return res.render('admin/profile');
   }
 };
