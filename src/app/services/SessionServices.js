7f5be63ea519103b9c5a4db639d8788bf8a492df
henryque <henryque_1@hotmail.com>
2021-08-14 19:38:18 -0300

    feat(SessionService): add forgot method as middleware for forgot-password post route

--- src/app/services/SessionServices.js
+++ src/app/services/SessionServices.js
@@ -10,6 +10,7 @@ async function login(req, res, next) {
 
   if (!user) return res.send('User does not already exists!');
 
+  // PASSWORD VALIDATION -- MODIFY
   const passed = '1234';
 
   if (!passed) return res.send('Invalid Password, please try again!');
@@ -19,6 +20,22 @@ async function login(req, res, next) {
   next();
 }
 
+async function forgot(req, res, next) {
+  const { email } = req.body;
+
+  const user = await User.findOne({
+    where: { email }
+  });
+
+  if (!user) return res.send('User does not exists!');
+
+  req.user = user;
+
+  next();
+
+}
+
 module.exports = {
-  login
+  login,
+  forgot
 }
