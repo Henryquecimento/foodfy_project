a46c1232fb7e253c502481d4c16a3d80d81901eb
henryque <henryque_1@hotmail.com>
2021-08-15 17:59:50 -0300

    feat(SessionController): add forgot method

--- src/app/controllers/privateAccess/SessionController.js
+++ src/app/controllers/privateAccess/SessionController.js
@@ -1,3 +1,6 @@
+const mailer = require('../../../lib/mailer');
+const crypto = require('crypto');
+const User = require('../../models/User');
 
 module.exports = {
   loginForm(req, res) {
@@ -15,6 +18,43 @@ module.exports = {
   },
   forgotForm(req, res) {
     return res.render('admin/session/forgot-password')
+  },
+  async forgot(req, res) {
+    const { user } = req;
+
+    try {
+      const token = crypto.randomBytes(20).toString('hex');
+
+      let now = new Date();
+      now = now.setHours(now.getHours() + 1);
+
+      await User.update(user.id, {
+        reset_token: token,
+        reset_token_expires: now
+      });
+
+      await mailer.sendMail({
+        to: user.email,
+        from: "no-reply@foodfy.com",
+        subject: "Recuperação de Senha",
+        html: `<h2>Esqueceu sua senha?</h2>
+        <p>Clique no link abaixo para resetar a senha</p>
+        <p>
+          <a href="http://localhost:5000/admin/password-reset?token=${token}" target="_blank">
+          RECUPERAR SENHA
+          </a>
+        </p>
+        `
+      });
+
+      return res.render('admin/session/forgot-password');
+
+    } catch (err) {
+
+      console.error(err);
+
+      return res.send('Erro inesperado!')
+    }
   }
 }
 
