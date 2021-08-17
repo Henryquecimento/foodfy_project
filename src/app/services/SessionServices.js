2f0ac426cf809bfca1ac03f5f250921f956fb57f
henryque <henryque_1@hotmail.com>
2021-08-17 18:51:24 -0300

    feat(SessionServices): add reset method to handle reset password validations

--- src/app/services/SessionServices.js
+++ src/app/services/SessionServices.js
@@ -11,7 +11,7 @@ async function login(req, res, next) {
   if (!user) return res.send('User does not already exists!');
 
   // PASSWORD VALIDATION -- MODIFY
-  const passed = '1234';
+  const passed = await compare(password, user.password);
 
   if (!passed) return res.send('Invalid Password, please try again!');
 
@@ -35,7 +35,31 @@ async function forgot(req, res, next) {
 
 }
 
+async function reset(req, res, next) {
+  const { email, password, passwordRepeat, token } = req.body;
+
+  const user = await User.findOne({
+    where: { email }
+  });
+
+  if (!user) return res.send('User does not exists!')
+
+  if (password != passwordRepeat) return res.send('Password Mismatch!', { token });
+
+  if (token != user.reset_token) return res.send('Invalid Token! Please ask a new Password Recovery', { token })
+
+  let now = new Date();
+  now = now.setHours(now.getHours());
+
+  if (now > user.reset_token_expires) return res.send('Token Expired! Please ask a new Password Recovery', { token });
+
+  req.user = user;
+
+  next();
+}
+
 module.exports = {
   login,
-  forgot
+  forgot,
+  reset
 }
