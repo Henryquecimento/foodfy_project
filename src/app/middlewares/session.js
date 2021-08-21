26673d7e81593c48f0e579cf57c33024d9f5bd9c
henryque <henryque_1@hotmail.com>
2021-08-21 19:38:19 -0300

    feat(middlewares/session): add userIsLogged method

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -6,6 +6,21 @@ function onlyUsers(req, res, next) {
   next();
 }
 
+function userIsLogged(req, res, next) {
+  if (req.session.userId) {
+    return res.redirect('/admin/users');
+  }
+
+  next();
+}
+
+// First, I'll have to send user.isAdmin to session
+/* function onlyAdmin(req, res, next) { 
+  if (!req.session.isAdmin)
+  next()
+} */
+
 module.exports = {
-  onlyUsers
+  onlyUsers,
+  userIsLogged
 }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = express.Router();
+
 const multer = require('../app/middlewares/multer');
-const session = require('../app/middlewares/session');
+const { onlyUsers, userIsLogged } = require('../app/middlewares/session');
+
 
 const RecipesController = require("../app/controllers/privateAccess/RecipesController");
 const ChefsController = require("../app/controllers/privateAccess/ChefsController");
@@ -13,19 +15,19 @@ const UserServices = require('../app/services/UserServices');
 const SessionServices = require('../app/services/SessionServices');
 /* -- Private Access -- */
 
-routes.get("/recipes", session.onlyUsers, RecipesController.index);
-routes.get("/recipes/create", session.onlyUsers, RecipesController.create);
-routes.get("/recipes/:id", session.onlyUsers, RecipesController.show);
-routes.get("/recipes/:id/edit", session.onlyUsers, RecipesController.edit);
+routes.get("/recipes", onlyUsers, RecipesController.index);
+routes.get("/recipes/create", onlyUsers, RecipesController.create);
+routes.get("/recipes/:id", onlyUsers, RecipesController.show);
+routes.get("/recipes/:id/edit", onlyUsers, RecipesController.edit);
 
 routes.post("/recipes", multer.array("photos", 5), RecipesController.post);
 routes.put("/recipes", multer.array("photos", 5), RecipesController.put);
 routes.delete("/recipes", RecipesController.delete);
 
-routes.get("/chefs", session.onlyUsers, ChefsController.index);
-routes.get("/chefs/create", session.onlyUsers, ChefsController.create);
-routes.get("/chefs/:id", session.onlyUsers, ChefsController.show);
-routes.get("/chefs/:id/edit", session.onlyUsers, ChefsController.edit);
+routes.get("/chefs", onlyUsers, ChefsController.index);
+routes.get("/chefs/create", onlyUsers, ChefsController.create);
+routes.get("/chefs/:id", onlyUsers, ChefsController.show);
+routes.get("/chefs/:id/edit", onlyUsers, ChefsController.edit);
 
 routes.post("/chefs", multer.array("photos", 5), ChefsController.post);
 routes.put("/chefs", multer.array("photos", 5), ChefsController.put);
@@ -34,7 +36,7 @@ routes.delete("/chefs", ChefsController.delete);
 /* -- USERS -- */
 
 // --- login/logout
-routes.get('/login', SessionController.loginForm);
+routes.get('/login', userIsLogged, SessionController.loginForm);
 routes.post('/login', SessionServices.login, SessionController.login);
 routes.post('/logout', SessionController.logout);
 
@@ -46,14 +48,14 @@ routes.post('/password-reset', SessionServices.reset, SessionController.reset);
 
 
 // Rotas de perfil de um usuário logado
-routes.get('/profile', session.onlyUsers, UserServices.show, ProfileController.index) // Mostrar o formulário com dados do usuário logado
-routes.put('/profile', session.onlyUsers, UserServices.update, ProfileController.put)// Editar o usuário logado
+routes.get('/profile', onlyUsers, UserServices.show, ProfileController.index) // Mostrar o formulário com dados do usuário logado
+routes.put('/profile', onlyUsers, UserServices.update, ProfileController.put)// Editar o usuário logado
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-routes.get('/users', session.onlyUsers, UserController.list) // Mostrar a lista de usuários cadastrados
-routes.get('/users/create', session.onlyUsers, UserController.create) // Mostrar o formulário de criação de um usuário
+routes.get('/users', onlyUsers, UserController.list) // Mostrar a lista de usuários cadastrados
+routes.get('/users/create', onlyUsers, UserController.create) // Mostrar o formulário de criação de um usuário
 routes.post('/users', UserServices.post, UserController.post) // Cadastrar um usuário
-routes.get('/users/:id/edit', session.onlyUsers, UserController.edit) // Mostrar o formulário de edição de um usuário
+routes.get('/users/:id/edit', onlyUsers, UserController.edit) // Mostrar o formulário de edição de um usuário
 routes.put('/users', UserController.put) // Editar um usuário
 routes.delete('/users', UserController.delete) // Deletar um usuário
 
