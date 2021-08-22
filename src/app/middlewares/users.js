51350ad959612db390e1e0b9141f25ff075a4de0
henryque <henryque_1@hotmail.com>
2021-08-22 18:02:18 -0300

    feat: add users middleware with onlyAdmin validation

---
+++ src/app/middlewares/users.js
@@ -0,0 +1,18 @@
+const User = require("../models/User");
+
+async function onlyAdmin(req, res, next) {
+  const result = await User.find(req.session.userId);
+
+  const isAdmin = result.rows[0].is_admin;
+
+  if (!isAdmin) {
+    return res.render('admin/users/index', {
+      error: 'Você não está autorizado a realizar esta operação!'
+    })
+  }
+
+}
+
+module.exports = {
+  onlyAdmin
+}
