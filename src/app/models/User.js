d25995a705f4c781882987e337128bef9cd84741
henryque <henryque_1@hotmail.com>
2021-09-27 22:33:20 -0300

    refactor(models/User): remove unnecessary methods

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,39 +1,7 @@
-const db = require("../../config/db");
-
 const Base = require('./Base');
 
 Base.init({ table: 'users' });
 
 module.exports = {
     ...Base,
-    async update(id, fields) {
-        let query = `UPDATE users SET`;
-
-        Object.keys(fields).map((key, index, array) => {
-
-            if ((index + 1) < array.length) {
-                query = `
-                    ${query}
-                    ${key} = '${fields[key]}',
-                `;
-            } else {
-                query = `
-                    ${query}
-                    ${key} = '${fields[key]}'
-                    WHERE id = ${id}                
-                `;
-            }
-        });
-
-        await db.query(query);
-
-        return;
-    },
-    delete(id) {
-
-        return db.query(`
-      DELETE FROM users WHERE id = $1
-      `, [id]);
-
-    }
 }
