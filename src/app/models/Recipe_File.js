8274dc32e47c04b85fceb79b1b68e7dbe77f6e2d
henryque <henryque_1@hotmail.com>
2021-10-01 18:24:00 -0300

    refactor(models/Recipe_File): remove unnecessary methods

--- src/app/models/Recipe_File.js
+++ src/app/models/Recipe_File.js
@@ -1,32 +1,7 @@
-const db = require('../../config/db');
 const Base = require('./Base');
 
 Base.init({ table: 'recipe_files' });
 
 module.exports = {
-  ...Base,
-  async create({ filename, path, recipe_id }) {
-    let query = `
-      INSERT INTO files (
-        name,
-        path
-      ) VALUES ($1, $2)
-      RETURNING id`;
-
-    let values = [filename, path];
-
-    // one file result
-    const result = await db.query(query, values);
-    const fileId = result.rows[0].id;
-
-    query = `
-    INSERT INTO recipe_files (
-      recipe_id,
-      file_id
-    )	VALUES ($1, $2)
-    `
-    values = [recipe_id, fileId]
-
-    return db.query(query, values);
-  }
+  ...Base
 }
