573fd3f333da02dea1e3b54d0763f32ec43652ef
henryque <henryque_1@hotmail.com>
2021-10-01 18:23:27 -0300

    refactor(models/File): remove unnecessary methods

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,41 +1,7 @@
-const db = require('../../config/db');
-const fs = require('fs');
-
 const Base = require('./Base');
 
 Base.init({ table: 'files' });
 
 module.exports = {
-	...Base,
-	async create({ filename, path, fieldname }) {
-		let query = `
-      INSERT INTO files (
-        name,
-        path
-      ) VALUES ($1, $2)
-      RETURNING id`;
-
-		let values = [filename, path];
-
-		// one file result
-		const result = await db.query(query, values);
-		const fileId = result.rows[0].id;
-
-		if (fieldname == "photos") {
-			return fileId;
-		}
-	},
-	async delete(id) {
-		try {
-			/* 			const results = await db.query('SELECT * FROM files WHERE id = $1', [id]);
-						const file = results.rows[0]
-			
-						fs.unlinkSync(file.path); */
-
-			return db.query(`DELETE FROM files WHERE id = $1`, [id]);
-
-		} catch (err) {
-			throw new Error(err);
-		}
-	}
+	...Base
 }
