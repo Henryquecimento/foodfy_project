e0a914964f22931e95046c683c74197c8640b958
henryque <henryque_1@hotmail.com>
2021-10-04 16:20:25 -0300

    refactor(PublicController): readjust methods to display refactores recipes methods

--- src/app/controllers/publicAccess/PublicController.js
+++ src/app/controllers/publicAccess/PublicController.js
@@ -1,17 +1,10 @@
-const Chef = require("../../models/Chef");
-const Recipe = require("../../models/Recipe");
-
 const { LoadChef } = require('../../services/LoadChefs');
 const { LoadRecipe } = require("../../services/LoadRecipes");
 
 module.exports = {
   async index(req, res) {
     try {
-      const recipes = await LoadRecipe.load('recipes', {
-        where: {
-          user_id: req.session.userId
-        }
-      });
+      const recipes = await LoadRecipe.load('recipes');
 
       return res.render("publicAccess/index", { recipes });
     } catch (err) {
@@ -23,18 +16,13 @@ module.exports = {
   },
   async show(req, res) {
     try {
-      let results = await Recipe.find(req.params.id);
-      let recipe = results.rows[0];
+      const recipe = await LoadRecipe.load('recipe', {
+        where: {
+          id: req.params.id
+        }
+      });
 
-      results = await Recipe.files(req.params.id);
-      let files = results.rows;
-
-      files = files.map(file => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
-      }));
-
-      return res.render("publicAccess/recipes/show", { recipe, files });
+      return res.render("publicAccess/recipes/show", { recipe });
     } catch (err) {
       throw new Error(err);
     }
@@ -53,22 +41,13 @@ module.exports = {
   },
   async showChef(req, res) {
     try {
-      let chef = await LoadChef.load('chef', { where: { id: req.params.id } })
+      const chef = await LoadChef.load('chef', { where: { id: req.params.id } })
 
-      let results = await Chef.findRecipe(req.params.id);
-      const recipes = results.rows;
-
-      for (recipe in recipes) {
-        results = await Recipe.files(recipes[recipe].id);
-        const file = results.rows[0];
-
-        recipes[recipe] = {
-          ...recipes[recipe],
-          filename: file.name,
-          src: `${req.protocol}://${req.headers.host}${file.path.replace('public', "")}`
+      const recipes = await LoadRecipe.load('recipes', {
+        where: {
+          chef_id: req.params.id
         }
-
-      }
+      });
 
       return res.render("publicAccess/chefs/show", { chef, recipes });
     } catch (err) {
