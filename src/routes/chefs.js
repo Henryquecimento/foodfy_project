8a6d669ab71418d888b711ee371136e53b96331d
henryque <henryque_1@hotmail.com>
2021-10-05 17:38:26 -0300

    refactor(routes/chefs): readjust routes to handle chefs validators

--- src/routes/chefs.js
+++ src/routes/chefs.js
@@ -5,15 +5,17 @@ const multer = require('../app/middlewares/multer');
 const { onlyUsers } = require('../app/middlewares/session');
 const { onlyAdmin } = require('../app/middlewares/users');
 
+const { post, update } = require('../app/validators/ChefsValidators');
+
 const ChefsController = require("../app/controllers/privateAccess/ChefsController");
 
-routes.get("/chefs", onlyUsers, ChefsController.index);
-routes.get("/chefs/create", onlyUsers, onlyAdmin, ChefsController.create);
-routes.get("/chefs/:id", onlyUsers, ChefsController.show);
-routes.get("/chefs/:id/edit", onlyUsers, onlyAdmin, ChefsController.edit);
+routes.get("/chefs", onlyUsers, ChefsController.index)
+  .get("/chefs/create", onlyUsers, onlyAdmin, ChefsController.create)
+  .get("/chefs/:id", onlyUsers, ChefsController.show)
+  .get("/chefs/:id/edit", onlyUsers, onlyAdmin, ChefsController.edit)
 
-routes.post("/chefs", onlyUsers, onlyAdmin, multer.array("photos", 1), ChefsController.post);
-routes.put("/chefs", onlyUsers, onlyAdmin, multer.array("photos", 1), ChefsController.put);
-routes.delete("/chefs", onlyUsers, onlyAdmin, ChefsController.delete);
+  .post("/chefs", onlyUsers, onlyAdmin, multer.array("photos", 1), post, ChefsController.post)
+  .put("/chefs", onlyUsers, onlyAdmin, multer.array("photos", 1), update, ChefsController.put)
+  .delete("/chefs", onlyUsers, onlyAdmin, ChefsController.delete);
 
 module.exports = routes;
