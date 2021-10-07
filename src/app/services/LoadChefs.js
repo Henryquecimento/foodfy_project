c3053735766750ff814eb1336bd508a856a0ec16
henryque <henryque_1@hotmail.com>
2021-10-07 19:09:58 -0300

    styles(LoadChefs): remove unnecessary comments

--- src/app/services/LoadChefs.js
+++ src/app/services/LoadChefs.js
@@ -25,7 +25,6 @@ async function format(chef) {
   }
 
   chef.img = files[0].src;
-  //avatarNamer-> para filename
   chef.filename = files[0].name;
   chef.filePath = files[0].path;
   chef.total_recipes = recipes.length;
