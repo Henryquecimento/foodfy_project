c58047c261d20e401c9d35c05d280046fcb6d982
henryque <henryque_1@hotmail.com>
2021-10-08 17:36:48 -0300

    feat(routes/recipes): add recipes validations in post and put methods

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,6 +5,8 @@ const multer = require('../app/middlewares/multer');
 const { onlyUsers } = require('../app/middlewares/session');
 const { onlyUserCreator } = require('../app/middlewares/users');
 
+const { post, update } = require('../app/validators/RecipesValidators');
+
 const RecipesController = require("../app/controllers/privateAccess/RecipesController");
 
 routes.get("/recipes", onlyUsers, RecipesController.index);
@@ -12,8 +14,8 @@ routes.get("/recipes/create", onlyUsers, RecipesController.create);
 routes.get("/recipes/:id", onlyUsers, RecipesController.show);
 routes.get("/recipes/:id/edit", onlyUsers, onlyUserCreator, RecipesController.edit);
 
-routes.post("/recipes", multer.array("photos", 5), RecipesController.post);
-routes.put("/recipes", multer.array("photos", 5), RecipesController.put);
+routes.post("/recipes", multer.array("photos", 5), post, RecipesController.post);
+routes.put("/recipes", multer.array("photos", 5), update, RecipesController.put);
 routes.delete("/recipes", RecipesController.delete);
 
 module.exports = routes;
